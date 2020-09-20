0f782bf6996fa694381f9c92d6bcb594fe79813b
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-09-20 21:35:30 +0530

    Update Count of Subset Sum Problem.cpp

--- Dynamic Programming/Count of Subset Sum Problem.cpp
+++ Dynamic Programming/Count of Subset Sum Problem.cpp
@@ -4,9 +4,7 @@ using namespace std;
 int countOfSubsetSum(vector<int>& arr, int sum, int n){
 	vector<vector<int> > dp(n+1, vector<int>(sum+1,0));
 	for(int i=0;i<n+1;i++){
-		for(int j=0;j<1;j++){
-			dp[i][j]=1;
-		}
+		dp[i][0]=1;
 	}
 	for(int i=1;i<n+1;i++){
 		for(int j=1;j<sum+1;j++){
