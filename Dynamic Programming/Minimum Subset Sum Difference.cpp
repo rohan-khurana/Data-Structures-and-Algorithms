88ebf200f25cb67f30b9aa4b498736e7a24dc887
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-09-20 21:34:09 +0530

    Update Minimum Subset Sum Difference.cpp

--- Dynamic Programming/Minimum Subset Sum Difference.cpp
+++ Dynamic Programming/Minimum Subset Sum Difference.cpp
@@ -4,9 +4,7 @@ using namespace std;
 vector<bool> subsetSum(vector<int>& arr, int sum, int n){
 	vector<vector<bool> > dp(n+1, vector<bool>(sum+1,false));
 	for(int i=0;i<n+1;i++){
-		for(int j=0;j<1;j++){
-			dp[i][j]=true;
-		}
+		dp[i][0]=true;
 	}
 	for(int i=1;i<n+1;i++){
 		for(int j=1;j<sum+1;j++){
