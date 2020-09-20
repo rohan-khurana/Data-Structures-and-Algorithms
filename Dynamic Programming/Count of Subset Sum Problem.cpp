be9e7b66c2184d7db334a2de9acd1ccbff240064
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-09-20 19:55:03 +0530

    Create Count of Subset Sum Problem.cpp

---
+++ Dynamic Programming/Count of Subset Sum Problem.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countOfSubsetSum(vector<int>& arr, int sum, int n){
+	vector<vector<int> > dp(n+1, vector<int>(sum+1,0));
+	for(int i=0;i<n+1;i++){
+		for(int j=0;j<1;j++){
+			dp[i][j]=1;
+		}
+	}
+	for(int i=1;i<n+1;i++){
+		for(int j=1;j<sum+1;j++){
+			if(arr[i-1]<=j){
+				dp[i][j]=dp[i-1][j-arr[i-1]] + dp[i-1][j];
+			}
+			else
+				dp[i][j]=dp[i-1][j];
+		}
+	}
+
+	return dp[n][sum];
+}
+
+int main(void){
+	int n,sum;
+	cout<<"Size:";
+	cin>>n;
+	vector<int> arr(n);
+	cout<<"Enter Elements:\n";
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	cout<<"Enter Sum:";
+	cin>>sum;
+	cout<<"\nNumber of Subsets Present: "<<countOfSubsetSum(arr,sum,n);
+	return 0;
+}
