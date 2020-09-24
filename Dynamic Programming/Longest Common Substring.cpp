accad73af5b11aaea2dd579dbe9ddf740b9e9623
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-09-24 23:24:56 +0530

    Create Longest Common Substring.cpp

---
+++ Dynamic Programming/Longest Common Substring.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestCommonSubstring(string X, string Y, int m, int n){
+	vector< vector<int> > dp(m+1 , vector<int>(n+1,0));
+	for(int i=1;i<m+1;i++){
+		for(int j=1;j<n+1;j++){
+			if(X[i-1]==Y[j-1])
+				dp[i][j]=1+dp[i-1][j-1];
+			else
+				dp[i][j] = 0;
+		}
+	}
+	return dp[m][n];
+}
+
+int main(void){
+	string X, Y;
+	cout<<"Enter String 1: ";
+	getline(cin,X);
+	cout<<"Enter String 2: ";
+	getline(cin,Y);
+	cout<<"\nLength of Longest Common Substring: "<<longestCommonSubstring(X,Y,X.length(),Y.length());
+	return 0;
+}
