0303748061b5e271b63497f09a67bdbf062c8fbd
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-10-21 01:55:21 +0530

    Create Longest Palindromic Subsequence.cpp

---
+++ Dynamic Programming/Longest Palindromic Subsequence.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int LPS(string X, string Y, int m, int n){
+	vector< vector<int> > dp(m+1 , vector<int>(n+1,0));
+	for(int i=1;i<m+1;i++){
+		for(int j=1;j<n+1;j++){
+			if(X[i-1]==Y[j-1])
+				dp[i][j]=1+dp[i-1][j-1];
+			else
+				dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+		}
+	}
+	
+	return dp[m][n];
+}
+
+int main(void){
+	string X, Y;
+	cout<<"Enter String: ";
+	getline(cin,X);
+	Y=string(X.rbegin(), X.rend());
+	cout<<"\nLength of Longest Palindromic Subsequence: "<<LPS(X,Y,X.length(),Y.length());
+	return 0;
+}
