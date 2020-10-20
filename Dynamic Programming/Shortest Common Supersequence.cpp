38acc2a3781e59e5be3a40fb9f8432f627e73af6
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-10-21 01:46:56 +0530

    Create Shortest Common Supersequence.cpp

---
+++ Dynamic Programming/Shortest Common Supersequence.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int shortestCommonSupersequence(string X, string Y, int m, int n){
+	vector< vector<int> > dp(m+1 , vector<int>(n+1,0));
+	for(int i=1;i<m+1;i++){
+		for(int j=1;j<n+1;j++){
+			if(X[i-1]==Y[j-1])
+				dp[i][j]=1+dp[i-1][j-1];
+			else
+				dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+		}
+	}
+	
+	return (m+n-dp[m][n]);
+}
+
+int main(void){
+	string X, Y;
+	cout<<"Enter String 1: ";
+	getline(cin,X);
+	cout<<"Enter String 2: ";
+	getline(cin,Y);
+	cout<<"\nLength of Shortest Common Supersequence: "<<shortestCommonSupersequence(X,Y,X.length(),Y.length());
+	return 0;
+}
