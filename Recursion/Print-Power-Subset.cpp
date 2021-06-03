9976ea64db4b7496d1ad5b22a278532c75c30254
Rohan Khurana <rohankhdbzmf@gmail.com>
2021-06-03 16:51:10 +0530

    Create Print-Power-Set.cpp

---
+++ Recursion/Print-Power-Subset.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+void printSubsets(string ip, string op){
+    if(ip.length()==0){
+        cout<<op<<" ";
+        return;
+    }
+    string op1=op;
+    string op2=op;
+    op2.push_back(ip[0]);
+    ip.erase(ip.begin()+0);
+
+    printSubsets(ip,op1);
+    printSubsets(ip,op2);
+    
+}
+
+using namespace std;aab
+int main(){
+    string ip;
+    string op="";
+    getline(cin,ip);
+    printSubsets(ip,op);
+}
