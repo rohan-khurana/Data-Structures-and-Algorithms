305406c099b771867bfc0cfc29c979b7b7a73348
Rohan Khurana <rohankhdbzmf@gmail.com>
2021-07-20 20:07:54 +0530

    Create Detect-Loop-in-LL.cpp

---
+++ Linked-Lists/Detect-Loop-in-LL.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int value;
+    Node* next;
+    Node(int value){
+        this->value=value;
+        this->next=NULL;
+    }
+};
+
+void insertAtEnd(Node** head, int val){
+    Node* node=new Node(val);
+    Node* last=*head;
+    if(*head==NULL){
+        *head=node;
+        return;
+    }
+    while(last->next!=NULL){
+        last=last->next;
+    }
+    last->next=node;
+    return;
+}
+
+bool detectLoop(Node* head){
+    Node* slowPtr=head;
+    Node* fastPtr=head;
+    while(slowPtr && fastPtr && fastPtr->next){
+        if(slowPtr->value==fastPtr->value){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    Node* head=NULL;
+    insertAtEnd(&head, 5);
+    insertAtEnd(&head, 8);
+    insertAtEnd(&head, 7);
+    insertAtEnd(&head, 9);
+    insertAtEnd(&head, 2);
+    head->next->next->next->next->next=head->next->next;
+    if(detectLoop(head)){
+        cout<<"Loop Exist";
+    }
+    else{
+        cout<<"No Loop Exist";
+    }
+    return 0;
+}
